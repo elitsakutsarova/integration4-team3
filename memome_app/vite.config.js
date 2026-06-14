d47c503f0ba983ee3d6aa61c3d2635b12e1a4ffa
elitsakutsarova <elitsa.biserova.kutsarova@student.howest.be>
2026-06-14 15:46:24 +0200

    Undid changes

--- memome_app/vite.config.js
+++ memome_app/vite.config.js
@@ -13,10 +13,6 @@ const hasCerts = fs.existsSync(keyFile) && fs.existsSync(certFile);
 
 export default defineConfig({
   plugins: [stickersManifestPlugin(), webrtcSignalingPlugin(), reactRouter(), tailwindcss()],
-  ssr: {
-    external: ['gsap', 'leaflet', 'maplibre-gl', 'react-leaflet'],
-    noExternal: [],
-  },
   server: {
     host: process.env.VITE_ALLOW_LAN === 'true' ? true : 'localhost',
     port: 5173,
